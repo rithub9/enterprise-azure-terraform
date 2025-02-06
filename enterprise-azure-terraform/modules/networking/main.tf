b1ff41e431cd8f78bf876ee3361c1fe4ce3cbc7c
ritHUB <Rishithumu27@gmail.com>
2025-02-06 00:09:25 -0500

    Networking Module (VNet + Subnets)

--- enterprise-azure-terraform/modules/networking/main.tf
+++ enterprise-azure-terraform/modules/networking/main.tf
@@ -1,1 +1,14 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "enterprise-vnet"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "app-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
